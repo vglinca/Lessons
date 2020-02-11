c08392e044cf771a44b4954c40cabd1af1f82ec7
Виталий Глинка <hataki29@outlook.com>
2020-02-11 17:50:34 +0200

    [Add]: lesson6

---
+++ lesson6/Lesson6/Lesson6/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            WorkWithOneDimensionalArray();
+            WorkWithMultidimensionalArrays();
+        }
+
+        static void WorkWithOneDimensionalArray()
+        {
+            //we can initialize array like this...
+            int[] arr = new int[10] { 5, 7, 3, 2, 7, 9, 1, 34, 5, 89 };
+            //...and like this
+            int[] arr1 = { 6, -4, 7, 9, -11, 10, -24, 34 - 46, 24 };
+
+            //sort in descending order
+            InsertionSort(arr, (a, b) => a < b);
+            PrintArr(arr);
+            //sort in ascending order
+            InsertionSort(arr, (a, b) => a > b);
+            PrintArr(arr);
+
+            InsertionSort(arr1, (a, b) => a > b);
+            PrintArr(arr1);
+        }
+
+        static void WorkWithMultidimensionalArrays()
+        {
+            int[,] m = new int[,] { 
+                {4,-1,4,6,7,2 }, 
+                {9,10,-13,0,5,0 }, 
+                { 7,1,4,-4,-7,4},
+                { 0,-3,7,8,-9,-1}, 
+                {-11,23,4,7,99,10 }, 
+                {89,46,-32,66,14,23 } 
+            };
+
+            PrintMatrix(m);
+
+            TransposeMatrix(m);
+            PrintMatrix(m);
+        }
+
+        static void InsertionSort(int[] arr, Func<int, int, bool> predicate)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = i; j > 0 && predicate(arr[j - 1], arr[j]); j--)
+                {
+                    Swap(ref arr[j], ref arr[j - 1]);
+                }
+            }
+        }
+
+        static void PrintArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("\n------------------------------------------------------------");
+        }
+
+        static void PrintMatrix(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write($"{m[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------------------------------------------------------");
+        }
+
+        static void TransposeMatrix(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = i; j < m.GetLength(1); j++)
+                {
+                    Swap(ref m[i, j], ref m[j, i]);
+                }
+            }
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
---
+++ lesson6/Lesson6/Strings/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //let's read some colors from a simple .txt file
+            string input = System.IO.File.ReadAllText(@"D:\dev\lesson6\Lesson6\Strings\input.txt");
+            Console.WriteLine($"String from \"input.txt\": {input}\n");
+            
+            WorkWithStrings(input);
+            WorkWithStringBuilder(input);
+        }
+
+        private static void WorkWithStringBuilder(string str)
+        {
+            var builder = new StringBuilder($"\"Colors: ");
+            
+            str = str.Trim();
+            var splitStr = str.Split(',');
+
+            int i = 0;
+            foreach (var s in splitStr)
+            {
+                i++;
+                var trimmedStr = s.Trim();
+                //if this is the last element of array, we append point
+                builder.Append(trimmedStr).Append(i == splitStr.Length ? "." : ", ");
+            }
+            builder.Append($"\"");
+
+            Console.WriteLine($"Builder: {builder}");
+        }
+
+        private static void WorkWithStrings(string input)
+        {
+            string buf = "";
+
+            //split input string from file by ','
+            var splitInput = input.Split(',');
+            foreach (var str in splitInput)
+            {
+                //trim each item to eleminate spaces
+                var trimmedStr = str.Trim();
+                
+                if (trimmedStr.StartsWith('b'))
+                {
+                    trimmedStr = trimmedStr.ToUpper();
+                }
+                //at the same time make some concatenation with buf to work with it later. Make space as delimeter
+                buf = String.Concat(buf, trimmedStr, ' ');
+                Console.WriteLine(trimmedStr);
+            }
+
+            //join split input and add ',' delimeter back and cw it.
+            var joinedString = string.Join(',', splitInput);
+            Console.WriteLine($"Joined string: {joinedString}\n");
+
+            //insert "Colors: " in from of joined string.
+            joinedString = joinedString.Insert(0, "Colors: ");
+            Console.WriteLine($"{joinedString}\n");
+
+            Console.WriteLine($"buf: {buf}\n");
+
+            //trim buf because there are may be space characters at the beginning and at the end of a string
+            buf = buf.Trim();
+
+            var splitBuf = buf.Split(' ');
+            foreach (var c in splitBuf)
+            {
+                var trimmedC = c.Trim();
+                trimmedC = trimmedC.ToLower();
+                Console.WriteLine($"Color\t\"{trimmedC}\"\n");
+            }
+
+            if(input.Contains("blue"))
+            {
+                Console.WriteLine("String contains \"blue\" color.\n");
+            }
+        }
+    }
+}
