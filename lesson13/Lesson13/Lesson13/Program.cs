edaf0d0323a933229f1c68eaba85e3fe74d990e2
Виталий Глинка <hataki29@outlook.com>
2020-02-24 17:22:26 +0200

    Task 14 finished.

--- lesson13/Lesson13/Lesson13/Program.cs
+++ lesson13/Lesson13/Lesson13/Program.cs
@@ -2,34 +2,29 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson13
 {
     class Program
     {
-        private static string dir1Path = @"D:\dev\Amdaris_lessons\dir1";
-        //@"D:\dir1";
-        private static string dir2Path = @"D:\dev\Amdaris_lessons\dir2";
-            //@"D:\dir2";
+        private static string dir1Path = //@"D:\dev\Amdaris_lessons\dir1";
+        @"D:\dir1";
+        private static string dir2Path = //@"D:\dev\Amdaris_lessons\dir2";
+            @"D:\dir2";
         static void Main(string[] args)
         {
             FileSystemWatcher watcher = new FileSystemWatcher();
 
-            DirectoryInfo dir1 = new DirectoryInfo(dir1Path);
-            DirectoryInfo dir2 = new DirectoryInfo(dir2Path);
-
             try
             {
                 watcher.Path = dir1Path;
-            } 
-            catch (Exception e)
+            } catch (Exception e)
             {
                 Console.WriteLine($"{e.Message}");
                 return;
             }
 
-            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | 
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite |
                 NotifyFilters.FileName | NotifyFilters.DirectoryName;
 
             watcher.Changed += OnChanged;
@@ -74,9 +69,9 @@ namespace Lesson13
             //retrieve name of changed file
             var fileName = Path.GetFileName(e.FullPath);
             //we wait until changed file is locked to prevent exception
-            //while (IsFileLocked(e.FullPath)) ;
-            while (FilesLockedInDirectory(dir1Path)) ;
-            
+            while (IsFileLocked(e.FullPath)) ;
+            //while (FilesLockedInDirectory(dir1Path)) ;
+
             //now we can go further
             Console.WriteLine($"File {fileName} is unlocked.");
             //compute hash for changed file from dir1
@@ -85,8 +80,8 @@ namespace Lesson13
             var dir2FilePath = Path.Combine(dir2Path, fileName);
             if (File.Exists(dir2FilePath) && dir1FileHash != null)
             {
-                //while (IsFileLocked(dir2FilePath)) ;
-                while (FilesLockedInDirectory(dir2Path)) ;
+                while (IsFileLocked(dir2FilePath)) ;
+                //while (FilesLockedInDirectory(dir2Path)) ;
                 //now we can compute hash for equivalent file in dir2
                 var dir2FileHash = CheckMD5(dir2FilePath);
                 //and check if file has been actually changed
@@ -95,13 +90,11 @@ namespace Lesson13
                     //delete file with old content and recreate it
                     File.Delete(dir2FilePath);
                     CreateFileInDir2(dir2FilePath, e.FullPath);
-                } 
-                else
+                } else
                 {
                     Console.WriteLine($"Nothing to change. File \"{dir2FilePath}\" is up to date.");
                 }
-            } 
-            else
+            } else
             {
                 Console.WriteLine($"Could not find file \"{fileName}\"");
             }
@@ -117,8 +110,7 @@ namespace Lesson13
             {
                 File.Delete(fileInDir2ToDelPath);
                 Console.WriteLine($"File \"{fileName}\" has successfully been deleted in {dir2Path}.");
-            } 
-            else
+            } else
             {
                 Console.WriteLine($"Could not find file \"{fileName}\" in {dir2Path}");
             }
@@ -129,21 +121,19 @@ namespace Lesson13
             //destPath is the full path of a new file in dir2 which will be created here.
             //srcPath is the full path of a created file in dir2
             //wait until file is locked by another process
-            //while (IsFileLocked(srcPath)) ;
-            //while (IsFileLocked(destPath)) ;
-            while (FilesLockedInDirectory(dir1Path)) ;
-            while (FilesLockedInDirectory(dir2Path)) ;
-            if (!IsTemporaryFile(srcPath))
+            while (IsFileLocked(srcPath)) ;
+            while (IsFileLocked(destPath)) ;
+            //while (FilesLockedInDirectory(dir1Path)) ;
+            //while (FilesLockedInDirectory(dir2Path)) ;
+            
+            using var inStream = new FileStream(srcPath, FileMode.OpenOrCreate);
+            using var reader = new StreamReader(inStream);
+            using var outStream = new FileStream(destPath, FileMode.Create);
+            using var writer = new StreamWriter(outStream);
+            while (!reader.EndOfStream)
             {
-                using var inStream = new FileStream(srcPath, FileMode.OpenOrCreate);
-                using var reader = new StreamReader(inStream);
-                using var outStream = new FileStream(destPath, FileMode.Create);
-                using var writer = new StreamWriter(outStream);
-                while (!reader.EndOfStream)
-                {
-                    var line = await reader.ReadLineAsync();
-                    await writer.WriteLineAsync(line);
-                }
+                var line = await reader.ReadLineAsync();
+                await writer.WriteLineAsync(line);
             }
         }
         private static void RenameFileInDir2(string oldPath, string newPath)
@@ -151,7 +141,7 @@ namespace Lesson13
             var newFileName = Path.GetFileName(newPath);
             var oldFileName = Path.GetFileName(oldPath);
             //additional checks for existing files in dir2
-            if(File.Exists(Path.Combine(dir2Path, oldFileName)) &&
+            if (File.Exists(Path.Combine(dir2Path, oldFileName)) &&
                 !File.Exists(Path.Combine(dir2Path, newFileName)))
             {
                 File.Move(Path.Combine(dir2Path, oldFileName), Path.Combine(dir2Path, newFileName));
@@ -167,18 +157,17 @@ namespace Lesson13
                 using var md5 = MD5.Create();
                 using var stream = File.OpenRead(filename);
                 return Encoding.Default.GetString(md5.ComputeHash(stream));
-            }
-            else
+            } else
             {
                 return null;
             }
         }
 
-        private static bool IsTemporaryFile(string filepath)
-        {
-            var extension = Path.GetExtension(filepath);
-            return extension == ".tmp";
-        }
+        //private static bool IsTemporaryFile(string filepath)
+        //{
+        //    var extension = Path.GetExtension(filepath);
+        //    return extension == ".tmp";
+        //}
 
         private static bool FilesLockedInDirectory(string directory)
         {
@@ -201,9 +190,8 @@ namespace Lesson13
                 {
                     using FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.None);
                     stream.Close();
-                } 
-            }
-            catch (Exception)
+                }
+            } catch (Exception)
             {
                 return true;
             }
--- lesson14/Lesson14/Lesson14/Program.cs
+++ lesson14/Lesson14/Lesson14/Program.cs
@@ -1,110 +1,179 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lesson14
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			dynamic dynamicVar = 5;
-			Console.WriteLine($"{nameof(dynamicVar)}: {dynamicVar}");
-			dynamicVar = "I almost like JavaScript!";
-			Console.WriteLine($"{nameof(dynamicVar)}: {dynamicVar}");
-			var workers = new List<Worker>
-			{
-				new Worker
-				{
-					FirstName = "John", LastName = "Smit", Age =45,HoursWorked = 120,Stage = 25
-				},
-				new Worker
-				{
-					FirstName = "Emy", LastName = "Farafauler",Age =31,HoursWorked = 140,Stage = 7
-				},
-				new Worker
-				{
-					FirstName = "Sheldon", LastName = "Cooper",Age =35,HoursWorked = 200,Stage = 20
-				},
-				new Worker
-				{
-					FirstName = "Hovard", LastName = "Volovits",Age =36,HoursWorked = 90,Stage = 10
-				},
-				new Worker
-				{
-					FirstName = "Ragesh", LastName = "Kutrapalli",Age =33,HoursWorked = 130,Stage = 9
-				},
-				new Worker
-				{
-					FirstName = "Andrew", LastName = "Jones",Age =47,HoursWorked = 150,Stage = 20
-				},
-				new Worker
-				{
-					FirstName = "Angela", LastName = "Mitchell",Age =39,HoursWorked = 145,Stage = 14
-				}
-			};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make dynamic var as integer
+            dynamic dynamicVar = 5;
+            //use nameof() to write variable name
+            Console.WriteLine($"{nameof(dynamicVar)}: {dynamicVar}");
+            //now assign dynamic variable with a string
+            dynamicVar = "I'm almost like JavaScript!";
+            Console.WriteLine($"{nameof(dynamicVar)}: {dynamicVar}");
 
-			foreach (var worker in workers)
-			{
-				worker.Salary = CalculateSalary(worker.Stage,
-					worker.HoursWorked > 135 ? true : false);
-				//using string interpolation
-				Console.WriteLine($"Worker: {worker.Name()} - Salary: {String.Format("{0:0.00}", worker.Salary)}.");
-			}
+            //trying to call unexisting method
+            //it's obvious that some exception will be thrown.
+            try
+            {
+                dynamicVar.Process();
+            } catch (Exception e)
+            {
+                Console.WriteLine($"{e.StackTrace}\nVariable {nameof(dynamicVar)} does not have method Process().");
+            }
 
-			foreach (var w in workers)
-			{
-				var mayHaveVacation = MayHaveVacation(stage: w.Stage, hoursWorked: w.HoursWorked, particularConditions: false);
-				Console.WriteLine($"Worker {w.Name()} {(mayHaveVacation ? "Can" : "Can't")} go to vacation.");
-			}
+            var workers = new List<Worker>
+            {
+                new Worker
+                { FirstName = "John", LastName = "Smit", Age =45,HoursWorked = 120,Stage = 25},
+                new Worker
+                { FirstName = "Emy", LastName = "Farafauler",Age =31,HoursWorked = 140,Stage = 7},
+                new Worker
+                { FirstName = "Sheldon", LastName = "Cooper",Age =35,HoursWorked = 200,Stage = 20},
+                new Worker
+                { FirstName = "Hovard", LastName = "Volovits",Age =36,HoursWorked = 90,Stage = 10},
+                new Worker
+                { FirstName = "Ragesh", LastName = "Kutrapalli",Age =33,HoursWorked = 130,Stage = 9},
+                new Worker
+                { FirstName = "Andrew", LastName = "Jones",Age =47,HoursWorked = 150,Stage = 20},
+                new Worker
+                { FirstName = "Angela", LastName = "Mitchell",Age =39,HoursWorked = 145,Stage = 14}
+            };
 
-		}
+            foreach (var worker in workers)
+            {
+                //call method with optional parameters
+                //we dont't specify 3d one, because it's optional.
+                worker.Salary = CalculateSalary(worker.Stage,
+                    worker.HoursWorked > 135 ? true : false);
+                //using string interpolation
+                Console.WriteLine($"Worker: {worker.Name()} - Salary: {String.Format("{0:0.00}", worker.Salary)}.");
+            }
 
-		//using optional parameters
-		static double CalculateSalary(int stage, bool hasPremium = false, bool hasTaxPrivileges = false)
-		{
-			var rnd = new Random();
-			var sal = 0.0;
-			for (int i = 1; i <= stage; i++)
-			{
-				sal += (stage % i) * 100;
-				if(stage > 20)
-				{
-					sal *= 1.3;
-				}
-			}
-			if (hasPremium)
-			{
-				sal += rnd.Next(5000, 6000);
-			}
-			if (hasTaxPrivileges)
-			{
-				sal += 2345;
-			}
-			return sal;
-		}
+            Console.WriteLine("------------------------------------------------------------------------");
 
-		static bool MayHaveVacation(double hoursWorked, int stage, bool particularConditions = false)
-		{
-			if(hoursWorked < 110 && stage < 10 && !particularConditions)
-			{
-				return false;
-			}
-			else if (hoursWorked > 135 && stage < 8 && !particularConditions)
-			{
-				return false;
-			}
-			return true;
-		}
-	}
+            //assign dynamic variable with workers collection
+            var filteredWorkers = workers.Where(w => w.HoursWorked < 180);
+            dynamicVar = filteredWorkers;
+            foreach (var w in dynamicVar)
+            {
+                //call method with named parameters
+                //this method has also some optional parameters
+                var mayHaveVacation = MayHaveVacation(stage: w.Stage, hoursWorked: w.HoursWorked, particularConditions: false);
+                //more string interpolation
+                Console.WriteLine($"Worker {w.Name()} {(mayHaveVacation ? "Can" : "Can't")} go to vacation.");
+            }
 
-	class Worker
-	{
-		public string FirstName { private get; set; }
-		public string LastName { private get; set; }
-		public string Name() => FirstName + LastName;
-		public int Age { get; set; }
-		public double HoursWorked { get; set; }
-		public int Stage { get; set; }
-		public double Salary { get; set; }
-	}
+            TypeConversions();
+            string str = "1";
+            FilterExceptions(str);
+        }
+
+        private static void TypeConversions()
+        {
+            IEnumerable<object> items = new List<object>
+            {
+                "some string variable",
+                123,
+                324.54,
+                new Worker{FirstName = "Vasya", LastName = "Ivanov", Age = 31 },
+                543,
+                Guid.NewGuid(),
+            };
+
+            foreach (var item in items)
+            {
+                if(item is int)
+                {
+                    Console.WriteLine($"Integer value found {item}");
+                    var str = item as string;
+                    str += " is a string.";
+                    Console.WriteLine($"Covert {item} to string. {item}");
+                }
+            }
+        }
+
+        private static void FilterExceptions(string str)
+        {
+            var param = str ?? throw new ArgumentNullException(nameof(str));
+            //enter two numbers
+            Console.Write("Enter dividend: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter divisor: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"You entered: x = {x} y = {y}");
+            //try to divide x by y
+            //and throw exception if devision by 0 has occured
+            try
+            {
+                var res = x / y;
+                Console.WriteLine($"x/y={res}");
+            } catch (DivideByZeroException ex) when (x > 0)
+            {
+                //if x > 0 then go here
+                Console.WriteLine($"{ex.StackTrace} and {nameof(x)} > 0");
+            } catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"{ex.StackTrace}");
+            }
+
+        }
+
+        //using optional parameters
+        static double CalculateSalary(int stage, bool hasPremium = false, bool hasTaxPrivileges = false)
+        {
+            //do some "extremely difficult" calculation logic
+            var rnd = new Random();
+            var sal = 0.0;
+            for (int i = 1; i <= stage; i++)
+            {
+                sal += (stage % i) * 100;
+                if (stage > 20)
+                {
+                    sal *= 1.3;
+                }
+            }
+            if (hasPremium)
+            {
+                sal += rnd.Next(5000, 6000);
+            }
+            if (hasTaxPrivileges)
+            {
+                sal += 2345;
+            }
+            return sal;
+        }
+
+        static bool MayHaveVacation(double hoursWorked, int stage, bool particularConditions = false)
+        {
+            if (hoursWorked < 110 && stage < 10 && !particularConditions)
+            {
+                return false;
+            } else if (hoursWorked > 135 && stage < 8 && !particularConditions)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    class Worker
+    {
+        //here we have some autoproperties
+        public string FirstName { private get; set; }
+        public string LastName { private get; set; }
+        //expression body method
+        //it returns concatenated FirstName and LastName
+        public string Name() => FirstName + LastName;
+        public int Age { get; set; }
+        public double HoursWorked { get; set; }
+        public int Stage { get; set; }
+        public double Salary { get; set; }
+    }
 }
